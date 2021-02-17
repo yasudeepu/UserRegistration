848006378514d3a8ca515cd2f33772fdc2390e3a
Yaswanthi <yasudeepu@gmail.com>
2021-02-17 14:57:16 -0600

    Second commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+
+
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+
+@Service
+@Transactional
+public class UserService {
+	
+	private final UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+	
+	public void saveMyUser(User user ) {
+		userRepository.save(user);
+	}
+	
+	public Iterable<User> showAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	public Iterable<User> deleteUserByUsername(String username) {
+		userRepository.deleteByUsername(username);
+		return userRepository.findAll();
+	}
+	
+	public Optional<User> editUser(int id) {
+		return userRepository.findById(id);
+	}
+	
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	}
